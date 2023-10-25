8f1ad7f2027f59b6ff2afde58c237da329a4e37b
Ahmad yahya almathami <90242708+oAmadu@users.noreply.github.com>
2023-10-25 22:41:00 +0300

    Update ANN.py

--- ANN.py
+++ ANN.py
@@ -26,3 +26,34 @@ wb = float(input("Enter value of the initial weight for bias: "))
 
 num_items = len(x_values)  # Number of items in x, y, bias
 all_errors_zero = False  # All errors aren't zero (So we have to start the algo)
+
+while not all_errors_zero:
+    all_errors_zero = True  # Assume all errors are zero
+
+    Err_values = []  # Reset the list for each iteration
+
+    for i in range(num_items):
+        S = x_values[i] * wx + y_values[i] * wy + bias[i] * wb
+        S_values.append(S)
+
+        if S > Thresh:
+            O = 1
+        else:
+            O = 0
+
+        Err = target[i] - O
+        wx = wx + LR * Err * x_values[i]
+        wy = wy + LR * Err * y_values[i]
+        wb = wb + LR * Err * bias[i]
+
+        Err_values.append(Err)  # Add the error to Err_values list
+        ic += 1  # add 1 for each iteration
+
+        if Err != 0:
+            all_errors_zero = False
+
+    if all(err == 0 for err in Err_values):
+        break
+
+    # Reset the S list for the next iteration
+    S_values = []
