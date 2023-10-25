6cd2570e1acc1a670a0e4795441b15759af1d26c
Ahmad yahya almathami <90242708+oAmadu@users.noreply.github.com>
2023-10-25 15:26:14 +0300

    Update ANN.py

--- ANN.py
+++ ANN.py
@@ -1,18 +1,28 @@
-
-
+Err_values = []
+target = []
 x_values = []
 y_values = []
 bias = []
+S_values = []
+ic = 0 # counting number of iterations
 
 while True:
-    x_input = input("Enter a value for x (or 'f' to finish): ")
+    x_input = input("Enter a value for x (or 'f' to finish): ") # enters X1 values
     if x_input == "f":
         break
-    y_input = input("Enter a value for y: ")
+    y_input = input("Enter a value for y: ") # Enters X2 values
+    target_input = input("Enter a value for the target: ")
 
-    x_values.append(x_input)
-    y_values.append(y_input)
-    bias.append(1)
+    x_values.append(float(x_input))
+    y_values.append(float(y_input))
+    bias.append(float(1))  # convert bias to float (all is 1's)
+    target.append(float(target_input))
 
-def iw(x,y,b):
-    output= (x * wx) + (y * wy) + (b * wb)
+Thresh = float(input("Enter the value of the threshold: ")) # Threshold input
+LR = float(input("Enter learning rate's value (if there isn't just enter 1): ")) #Learning rate input
+wx = float(input("Enter value of the initial weight for x: "))
+wy = float(input("Enter value of the initial weight for y: "))
+wb = float(input("Enter value of the initial weight for bias: "))
+
+num_items = len(x_values)  # Number of items in x, y, bias
+all_errors_zero = False  # All erors aren't zero (So we have to start the algo)
